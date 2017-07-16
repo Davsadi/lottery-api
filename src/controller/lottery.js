99fa1c492f7c0872c3778e938eef1d33c8e17c71
David Ferguson <git@dferguson.com>
2017-07-15 17:34:17 -0700

    Added ordering to get all lottery call

--- src/controller/lottery.js
+++ src/controller/lottery.js
@@ -45,7 +45,7 @@ export default({ config, db }) => {
     // '/v1/lottery/' - Read
     //Get all lotteries, orded by drawDate descending
     api.get('/', (req, res) => {
-        Lottery.find({}, null, {sort: {drawDate: -1}, (err, lotterys) => {
+        Lottery.find({}, null, {sort: {drawDate: -1}}, (err, lotterys) => {
             if (err) {
                 res.send(err);
             }
